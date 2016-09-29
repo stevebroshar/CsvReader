66ec94926e41742f8ff76589634116febf6749c9
Steven <Steven@BROSHARDELL4>
2016-09-28 20:16:12 -0500

    Add project files.

---
+++ CsvReader/CsvReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvReader
+{
+    public sealed class CsvReader
+    {
+        private readonly TextReader _textReader;
+
+        public CsvReader(TextReader textReader)
+        {
+            _textReader = textReader;
+        }
+
+        public static CsvReader Parse(string text)
+        {
+            return new CsvReader(new StringReader(text));
+        }
+
+        /// <summary>
+        /// Returns the next record of values or null if the end of the stream has been reached.
+        /// </summary>
+        public IEnumerable<string> ReadRecord()
+        {
+            string text = _textReader.ReadLine();
+            return text?.Split(',');
+        }
+    }
+}
---
+++ CsvReaderUnitTest/CsvReaderUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using CsvReader;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
+
+namespace CsvReaderUnitTest
+{
+    [TestClass]
+    public class CsvReaderUnitTest
+    {
+        [TestMethod]
+        public void ReadRecord_ReturnsValuesDelimitedByComma()
+        {
+            var reader = CsvReader.CsvReader.Parse("a,b,c");
+            CollectionAssert.AreEqual(new[] { "a", "b", "c" }, reader.ReadRecord().ToArray());
+        }
+
+        [TestMethod]
+        public void ReadRecord_ReturnsNullAfterLastLine()
+        {
+            var reader = CsvReader.CsvReader.Parse("a,b,c");
+            reader.ReadRecord();
+            Assert.IsNull(reader.ReadRecord());
+        }
+    }
+}
